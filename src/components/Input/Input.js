917a7701a68d72c2c583e0de9fa91849ac78f233
zeroboycoder <pyaesonekhant@tumawlamyine.edu.mm>
2022-03-27 18:27:39 +0630

    Finish Create Staff

---
+++ src/components/Input/Input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {TextField} from '@mui/material'
+
+const Input = (props) => {
+    return <TextField
+        fullWidth
+        required
+        id={props.id}
+        value={props.value}
+        onChange={(e) => props.changed(props.id, e.target.value)}
+    />
+}
+
+export default Input;
